5a941dd670e1b2b5b0dfb8a89bf5bba7623e4561
dimamaisoftwire <dima.mai@softwire.com>
2025-07-07 15:00:03 +0100

    added waterbomb functionality

--- src/bots/randoBotWater.ts
+++ src/bots/randoBotWater.ts
@@ -2,7 +2,23 @@ import { Gamestate, BotSelection } from '../models/gamestate';
 
 class Bot {
     dynamiteRemaining = 100;
+    oppDynamiteCount = 0;
+    opponentDrawResponses = [];
+    checkDynamiteDrawResponse() : boolean {
+        let setOfResponses = new Set(this.opponentDrawResponses);
+        if(setOfResponses.size==1 && this.opponentDrawResponses.length>1){
+            if(setOfResponses[0] == "D"){
+                return true;
+            }
+        }
+        return false;
+    }
     makeMove(gamestate: Gamestate): BotSelection {
+        // Keeping track of opponents dynamite
+        if (gamestate.rounds.length > 0 && gamestate.rounds[gamestate.rounds.length - 1].p2 === 'D'){
+            this.oppDynamiteCount ++;
+        }
+        // Check if opponent kept doing dynamite last N moves
         function checkLastMoves(n:number, move:BotSelection){
             if(gamestate.rounds.length < n) return false;
             let last = gamestate.rounds[gamestate.rounds.length-1].p2;
@@ -13,43 +29,25 @@ class Bot {
             }
             return true;
         }
-        function checkOpponentDrawResponse(){
-            // Checks if opponent has same response to draws
-            if(gamestate.rounds.length<2) return false;
-            let responses: BotSelection[] = [];
-            for(let i = gamestate.rounds.length-2; i > 0; i--){
-                if(gamestate.rounds[i].p1 === gamestate.rounds[i].p2){ // Get all of oponents moves after draw
-                    responses.push(gamestate.rounds[i+1].p2);
-                }
-            }
-            let setofResponse = new Set(responses);
-            console.log(setofResponse)
-            return (setofResponse.size === 1);
-
-        }
         function checkLastDraw(){ // Check if last game was draw
             if(gamestate.rounds.length===0) return true;
             const lastMove = gamestate.rounds.length-1;
             return (gamestate.rounds[lastMove].p1 === gamestate.rounds[lastMove].p2);
         }
-        function checkDrawDynamiteOpponent(){
-            if(gamestate.rounds.length <= 1) return true;
-            // Finding last draw
-            for(let i=gamestate.rounds.length-2;i>=0;i--){
-                if(gamestate.rounds[i].p2 == gamestate.rounds[i].p1){
-                    console.log(gamestate.rounds[i+1].p2 === "D");
-                    return (gamestate.rounds[i+1].p2 === "D"); // check that the opponents move after draw was dynamite
-                }
+        // Keeping track of opponents draw responses
+        if(gamestate.rounds.length > 1){
+            if(gamestate.rounds[gamestate.rounds.length-2].p1 == gamestate.rounds[gamestate.rounds.length-2].p2){
+                this.opponentDrawResponses.push(gamestate.rounds[gamestate.rounds.length-1]);
             }
-            return false;
         }
 
         const baseMoves: BotSelection[] = ['R', 'P' ,'S'];
         let selection = baseMoves[Math.floor(Math.random() * baseMoves.length)];
+
         if(checkLastDraw()&&this.dynamiteRemaining>0){
             selection = "D";
         }
-        if(checkLastMoves(3,"D") || checkOpponentDrawResponse()){
+        if(checkLastMoves(3,"D") || this.checkDynamiteDrawResponse()){
             selection = "W";
         }
         if(selection == "D"){
